7f1b03120353626409c440d428446cc4f18bfe22
Kasun Eranda <kasuneranda2012@gmail.com>
2025-06-14 19:49:20 +0530

    Updated main.cpp with test for MotorController class

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 #include <unistd.h>
 #include "drivers/TemperatureSensor/TemperatureSensor.hpp"
+#include "drivers/MotorController/MotorController.hpp"
 
 TemperatureSensor ts;
+MotorController mc;
 
 int main(int argc, char* argv[]) {
     std::cout << "Plexus Test #1" << std::endl;
 
     ts = TemperatureSensor();
+    
+    mc.setSpeed(200); // Set a fixed speed for testing
+
     while(1)
     {
         double _temp = 0.00;
         ts.updateTemperature();
         ts.getTemperature(&_temp);
 
+        std::int16_t _motorSpeed = 0;
+        mc.readMotorSpeed();
+        mc.getSpeed(&_motorSpeed);
+
         std::cout << "Temperature: " << _temp << endl;
+        std::cout << "Motor Speed: " << _motorSpeed << " RPM" << std::endl;
+        std::cout << endl;
         sleep(1);
     }
     
