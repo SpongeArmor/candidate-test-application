6815d6730b4df7d3cefaead423bd8031a9553c4b
Kasun Eranda <kasuneranda2012@gmail.com>
2025-06-14 13:37:54 +0530

    Added demo code for TemperatureSensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <unistd.h>
+#include "drivers/TemperatureSensor/TemperatureSensor.hpp"
+
+TemperatureSensor ts;
 
 int main(int argc, char* argv[]) {
     std::cout << "Plexus Test #1" << std::endl;
 
+    ts = TemperatureSensor();
+    while(1)
+    {
+        double _temp = 0.00;
+        ts.updateTemperature();
+        ts.getTemperature(&_temp);
+
+        std::cout << "Temperature: " << _temp << endl;
+        sleep(1);
+    }
+    
     return 0;
 }
