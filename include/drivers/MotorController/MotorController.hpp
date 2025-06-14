6cdf1ea88aeefc8cefc88411d0ec6c9619ae5530
Kasun Eranda <kasuneranda2012@gmail.com>
2025-06-14 19:47:02 +0530

    Created base driver MotorController with RPM control

---
+++ include/drivers/MotorController/MotorController.hpp
@@ -0,0 +1,64 @@
+#ifndef MOTOR_CONTROLLER_HPP
+#define MOTOR_CONTROLLER_HPP
+#include "errors.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <cstdint>
+#include <random>
+
+#define MOTOR_MAX_SPEED 3000 // Maximum speed in RPM
+// #define GENERATE_DUMMY_DATA // uncomment to be able to set the motor speed manually for testing purposes
+
+constexpr std::int16_t _min_speed = 450; // Minimum temperature in Celsius for randim value generation
+constexpr std::int16_t _max_speed = 550; // Maximum temperature in Celsius for random value generation
+constexpr float _min_current = 1.1; 
+constexpr float _max_current = 1.3;
+static_assert(_max_speed > _min_speed, "Invalid reference temprature range provided, please check again.");
+
+typedef struct {
+    std::uint8_t pin_pwm;        // PWM pin for motor control
+    std::uint8_t pin_direction;  // Direction control pin
+    std::uint8_t pin_enable;     // Enable pin for the motor
+    std::uint16_t max_speed;     // Maximum speed in RPM
+} motor_config_t;
+
+
+enum Direction {
+    FORWARD,
+    REVERSE
+};
+
+typedef struct {
+    std::int16_t speed;          // Current speed of the motor in RPM
+    float current;               // Current drawn by the motor in Amperes
+    std::uint8_t status;         // Status of the motor (e.g., running, stopped)
+} motor_data_t;
+
+using namespace std;
+
+class MotorController {
+    private:
+        bool _initialized = false;
+        bool _motorRunning = false;
+        Direction _motorDirection = Direction::FORWARD;
+        std::int16_t _motorSpeed = 0; // Current speed of the motor in RPM
+        motor_config_t _motorConfig = {0, 0, 0, MOTOR_MAX_SPEED}; // Default motor configuration
+        
+        ErrorCode initializeMotor(motor_config_t motorconfig);
+        ErrorCode generateMotorData(motor_data_t *motorData);
+        
+        public:
+        MotorController();
+        ~MotorController();
+        
+        ErrorCode setSpeed(std::int16_t speed);
+        ErrorCode readMotorSpeed(void);
+        ErrorCode getSpeed(std::int16_t *speed);
+
+        ErrorCode startMotor(std::int16_t speed, Direction direction = Direction::FORWARD);
+        ErrorCode setDirection(Direction direction);
+        ErrorCode stopMotor(void);
+        
+};
+
+#endif // MOTOR_CONTROLLER_HPP
---
+++ src/drivers/MotorController/MotorController.cpp
@@ -0,0 +1,146 @@
+#include "drivers/MotorController/MotorController.hpp"
+
+MotorController::MotorController() {
+    if (!_initialized) {
+        _motorRunning = 0;
+        _initialized = true;
+        std::cout << "MotorController initialized." << std::endl;
+    } else {
+        std::cout << "MotorController already initialized." << std::endl;
+    }
+}
+
+MotorController::~MotorController() {
+}
+
+ErrorCode MotorController::initializeMotor(motor_config_t motorconfig){
+    if(!_initialized)
+    {
+        return ErrorCode::OBJECT_NOT_INITIALIZED;
+    }
+
+    // Initialize motor here i.e. set up GPIO pins, PWM, etc.
+
+    return ErrorCode::OK;
+}
+
+ErrorCode MotorController::setDirection(Direction direction) {
+    if (!_initialized) {
+        return ErrorCode::OBJECT_NOT_INITIALIZED;
+    }
+
+    _motorDirection = direction;
+
+    // Set the motor direction here
+
+    return ErrorCode::OK;
+}
+
+ErrorCode MotorController::setSpeed(std::int16_t speed) {
+    if (!_initialized) {
+        return ErrorCode::OBJECT_NOT_INITIALIZED;
+    }
+
+    if (speed < 0 || speed > _motorConfig.max_speed) {
+        return ErrorCode::INACCURATE_SENSOR_DATA; // Assuming this is the closest error code
+    }
+
+#ifndef GENERATE_DUMMY_DATA
+    _motorSpeed = speed;
+#endif
+
+    // Set the motor speed here using PWM
+
+    return ErrorCode::OK;
+}
+
+ErrorCode MotorController::startMotor(std::int16_t speed, Direction direction){
+    if (!_initialized) {
+        return ErrorCode::OBJECT_NOT_INITIALIZED;
+    }
+
+    if (speed < 0 || speed > _motorConfig.max_speed) {
+        return ErrorCode::INVALID_VALUE; // Assuming this is the closest error code
+    }
+
+    _motorSpeed = speed;
+    _motorDirection = direction;
+    
+    // Start the motor here using PWM and set direction
+    
+    _motorRunning = true;
+
+    return ErrorCode::OK;
+}
+
+/**
+ * Retrieve the last updated motor speed from the object.
+ * @return ErrorCode indicating success or failure.
+ * @param Pointer to store the current speed, <std::int16_t>.
+ */
+ErrorCode MotorController::getSpeed(std::int16_t *speed) {
+    if (!_initialized) {
+        return ErrorCode::OBJECT_NOT_INITIALIZED;
+    }
+
+    *speed = this->_motorSpeed;
+
+    return ErrorCode::OK;
+}
+
+/**
+ * Read motor speed and update the internal state.
+ * @param None
+ * @return ErrorCode indicating success or failure.
+ * @note This function should be called periodically to refresh the motor speed data from the sensor. Assumed to produce value in revolutions per minute (RPM).
+ */
+ErrorCode MotorController::readMotorSpeed(void){
+    if(!_initialized)
+        return ErrorCode::OBJECT_NOT_INITIALIZED;
+
+    motor_data_t _dummy_motor_data;
+
+    // Generate dummy data/read actual sensor data
+    // In this case, we will generate dummy data
+    ErrorCode _error;
+
+#ifdef GENERATE_DUMMY_DATA
+    _error = generateMotorData(&_dummy_motor_data);  // to generate random data within a given rage
+    this->_motorSpeed = _dummy_motor_data.speed;
+#endif
+
+    if(_error != ErrorCode::OK) {
+        std::cout << "Error generating motor data: " << static_cast<int>(_error) << endl;
+        return _error;
+    }
+
+    return ErrorCode::OK;
+}
+
+/**
+ * Generates random motor data for testing purposes.
+ * @param motorData Pointer to the motor_data_t structure to fill with data.
+ * @return ErrorCode indicating success or failure.
+ * @note This function simulates the behavior of a motor by generating random values.
+ */
+ErrorCode MotorController::generateMotorData(motor_data_t *motorData) {
+    std::int16_t _speed = 0;
+    float _current = 0.0;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::uniform_int_distribution<> _dist_speed(_min_speed, _max_speed);
+    std::uniform_real_distribution<> _dist_current(_min_current, _max_current);
+    _speed = _dist_speed(gen);
+    _current = _dist_current(gen);
+
+    if(_speed > _max_speed || _speed < _min_speed)
+        return ErrorCode::INACCURATE_SENSOR_DATA;
+
+    motorData->speed = _speed;
+    motorData->current = _current;
+    motorData->status = _motorRunning ? 1 : 0;
+
+    return ErrorCode::OK;
+}
